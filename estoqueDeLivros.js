40b5dbaf0a4535115ea016635b5ccc693f36cee5
sameladuart <samelamica@gmail.com>
2024-10-16 20:46:00 -0400

    alteração

--- estoqueDeLivros.js
+++ estoqueDeLivros.js
@@ -16,7 +16,7 @@ const porta = 3334  // estou criando a porta
 async function mostraLivro (request,response) {
 try {
 
-    const livrosVindosDoBancoDeDados = await Mulher.find()
+    const livrosVindosDoBancoDeDados = await Livro.find()
     response.json(livrosVindosDoBancoDeDados)
 } catch(erro){
     console.log(erro)
@@ -69,7 +69,7 @@ async function corrigeLivro (request,response) {
 async function deletaLivro (request,response) {
     try {
     await Livro.findByIdAndDelete(request.params.id)
-    response.json({message: "mulher deletada com sucesso!"})
+    response.json({message: "livro deletado com sucesso!"})
     } catch(erro){
         console.log(erro)
     }
